dbe956b8823090d5814ac8c69519885a7095d62c
lkw657 <lkw657@users.noreply.github.com>
2017-01-21 23:05:20 +0800

    Registered models with admin site

--- logbook/admin.py
+++ logbook/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+for model in [LBUser, Organisation, Supervisor, LogBook, Category, LogEntry]:
+    admin.site.register(model)
