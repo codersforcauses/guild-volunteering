a919906d133b8eb1a473325e7e1b09ce193e761e
lkw657 <lkw657@users.noreply.github.com>
2017-01-21 22:57:45 +0800

    Separated database settings into another file

---
+++ README.md
@@ -0,0 +1,22 @@
+# Guild Volenteering
+An alternative to physical logbooks for Guild Volenteering UWA to allow students to enter their voulnteering hours .
+
+The django project requires that some local setting be configured for database access. To do this create a new file in the gvs directory called `local_settings.py`
+**Example contents for a mysql server:**
+
+```
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DatabaseName',
+        'USER': 'DatabaseUser',
+        'PASSWORD': 'DatabasePassword',
+        'HOST': '127.0.0.1'
+    }
+}
+ALLOWED_HOSTS = ['127.0.0.1']
+```
+`ALLOWED_HOSTS` should be configured to a list of domains that the server is allowed to serve [more information](https://docs.djangoproject.com/en/1.10/ref/settings/)
+Additional database settings and engines can be found [here](https://docs.djangoproject.com/en/1.10/ref/settings/#std:setting-DATABASES)
+
+For development the line `DEBUG = True` can be added to the file.
--- gvs/settings.py
+++ gvs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5de1_txz%-chu&9of#e(ygn_f5%=c9#!-ym_j_sj-^v@j@qj!a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -73,12 +74,12 @@ WSGI_APPLICATION = 'gvs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
@@ -118,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('Error: Cannot find local_settings.py\nPlease refer to the readme')
+    sys.exit()
+
+if 'DATABASES' not in globals():
+    print('Error: Database not configured in local_settings.py\nPlease refer to the readme')
+    sys.exit()
