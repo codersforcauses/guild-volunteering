3c37bb9aa2b2f85bfe45e606a947258ad7f3fd1e
lkw657 <lkw657@users.noreply.github.com>
2017-01-22 23:09:36 +0800

    Added signup and login forms

--- logbook/urls.py
+++ logbook/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
-    url(r'^login/$', views.login, name = 'login'),
-    url(r'^logout/$', views.logout, name = 'logout'),
+    url(r'^$', views.indexView, name = 'index'),
+    url(r'^signup/$', views.signupView, name = 'signup'),
+    url(r'^login/$', views.loginView, name = 'login'),
+    url(r'^logout/$', views.logoutView, name = 'logout'),
     ]
