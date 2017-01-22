8ab80057aecdc5e3d2ffbb66efb9ce76b3f3c66a
Daedalos97 <sam.j.s.heath@gmail.com>
2017-01-22 14:47:27 +0800

    Updated the admin page to include additional fields

--- logbook/admin.py
+++ logbook/admin.py
@@ -1,25 +1,37 @@
 from django.contrib import admin
 from .models import *
 
-# These only have one field
-for model in [LBUser, Category]:
-    admin.site.register(model)
+@admin.register(LBUser)
+class LBUserAdmin(admin.ModelAdmin):
+    list_display = ['user','first_name','last_name', 'email',]
 
+    def first_name(self, obj):
+        return obj.user.first_name
+
+    def last_name(self, obj):
+        return obj.user.last_name
+
+    def email(self,obj):
+        return obj.user.email
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(LogBook)
 class LogBookAdmin(admin.ModelAdmin):
     list_display = ['user','name','description','created_at','updated_at',]
 
+@admin.register(LogEntry)
 class LogEntryAdmin(admin.ModelAdmin):
     list_display = ['book','category','description','supervisor','start','end',
                     'created_at','updated_at','status',]
-
+@admin.register(Organisation)
 class OrganisationAdmin(admin.ModelAdmin):
     # Will need to add the Org's Calista code when we add that to model
     list_display = ['name',]
 
+@admin.register(Supervisor)
 class SupervisorAdmin(admin.ModelAdmin):
     list_display = ['user','email','validated','organisation',]
 
-admin.site.register(LogBook, LogBookAdmin)
-admin.site.register(LogEntry, LogEntryAdmin)
-admin.site.register(Organisation, OrganisationAdmin)
-admin.site.register(Supervisor, SupervisorAdmin)
