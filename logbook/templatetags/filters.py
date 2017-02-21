412148e62d9f8b790490800d09896da1d7a472bf
Daedalos97 <sam.j.s.heath@gmail.com>
2017-02-21 23:08:28 +0800

    Forgot to remove arg in filter

--- logbook/templatetags/filters.py
+++ logbook/templatetags/filters.py
@@ -5,7 +5,7 @@ register = template.Library()
 import datetime
 
 @register.filter(name='timedelta')
-def timedelta(value, arg):
+def timedelta(value):
     seconds = value.total_seconds()
     hours = int(seconds/3600.0)
     minutes = int((seconds - (hours*3600))/60.0)
