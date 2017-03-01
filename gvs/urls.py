82889966a310b9ace81f7c7415d872dbba32c59b
Samuel Heath <sam.j.s.heath@gmail.com>
2017-03-01 16:34:25 +0800

    Added template files for reset password

--- gvs/urls.py
+++ gvs/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^logbook/accounts/', include('django.contrib.auth.urls')),
     url(r'^logbook/', include('logbook.urls')),
 ]
