b829ede02d779f69c9f6e103721968723c7d4259
Samuel Heath <sam.j.s.heath@gmail.com>
2017-03-07 10:09:15 +0800

    Updated admin output
    Now outputs for a given student, organisation or both

--- logbook/admin.py
+++ logbook/admin.py
@@ -42,25 +42,50 @@ class OrganisationAdmin(admin.ModelAdmin):
 class SupervisorAdmin(admin.ModelAdmin):
     list_display = ['user','email','validated','organisation',]
 
+def getFileName(user, organisation):
+    if not user == None and not organisation == None:
+    if not user == None:
+        return user.user.username
+    else:
+        return organisation.name
+
 @admin.site.register_view(r'logbook/export', visible='false', name="Export Logbooks")
 def exportView(request):
     DATE_FORMAT = '%d/%m/%Y'
     if request.method == 'POST':
         form = ExportForm(request.POST)
+        
         if form.is_valid():
+            
             response = HttpResponse(content_type='text/csv')
             user = form.cleaned_data['student']
-            response['Content-Disposition'] = 'attachment; filename="{}.csv"'.format(user.user.username)
+            organisation = form.cleaned_data['organisation']
+            
+            response['Content-Disposition'] = 'attachment; filename="{}.csv"'.format(getFileName(user,organisation))
             writer = csv.writer(response)
             writer.writerow(['StudentID','First Name','Surname','Calendar Year','Position Type','Host Organisation Code','Host Organisation Description','Start Date','End Date','Hours'])
+
             fields = ['book__user__user__username', 'book__user__user__first_name','book__user__user__last_name', 'year', 'book__category__name', 'book__organisation__code','book__organisation__name']
-            entries = LogEntry.objects.filter(book__user=user, status='Approved'
+            entries = []
+            
+            if not user == None and not organisation == None:
+                entries = LogEntry.objects.filter(book__organisation__exact = organisation,book__user = user, status='Approved'
                     ).extra(select={'year': "EXTRACT(year FROM start)"}
                     ).values(*fields
                     ).annotate(startDate=Min('start'), endDate=Max('end'))
-            print(entries)
+                print(entries)
+            elif not user == None:
+                entries = LogEntry.objects.filter(book__user = user,status='Approved'
+                    ).extra(select={'year': "EXTRACT(year FROM start)"}
+                    ).values(*fields
+                    ).annotate(startDate=Min('start'), endDate=Max('end'))
+            else:
+                entries = LogEntry.objects.filter(book__organisation = organisation,status='Approved'
+                    ).extra(select={'year': "EXTRACT(year FROM start)"}
+                    ).values(*fields
+                    ).annotate(startDate=Min('start'), endDate=Max('end'))
+            
             for entry in entries:
-                print(entry,'\n', entry['year'])
                 writer.writerow([entry[f] for f in fields]
                         + [ entry['startDate'].strftime(DATE_FORMAT), entry['endDate'].strftime(DATE_FORMAT), round((entry['endDate']-entry['startDate']).seconds/3600)])
             return response
--- logbook/adminForms.py
+++ logbook/adminForms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import *
 
 class ExportForm(forms.Form):
-    student = forms.ModelChoiceField(queryset = LBUser.objects.filter(user__groups__name='LBStudent').all(), label='Student')
-    #organisation = forms.ModelChoiceField(queryset = Organisation.objects.all(), label='Organisation')
-
+    organisation = forms.ModelChoiceField(queryset = Organisation.objects.all().order_by('name'),
+                                          label='Organisation',widget=forms.Select(attrs={'class':'form-control'}), required=False)
+    student = forms.ModelChoiceField(queryset = LBUser.objects.filter(user__groups__name='LBStudent').all().order_by('user__username'),
+                                     label='Student',widget=forms.Select(attrs={'class':'form-control'}), required=False)
